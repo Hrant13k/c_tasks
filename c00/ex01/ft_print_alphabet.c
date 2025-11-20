bf1da97a7a0bb7f5c765cc0cf6b1c9537afa84fe
Hrant <hrantk1313@gmail.com>
2025-11-20 23:29:34 +0400

    1st five files

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+
+	//while letter is less than or equal to z
+	while (letter <= 'z')
+	{
+		//print letter
+		write(1, &letter, 1);
+
+		//go to next letter
+		letter++;
+	}
+}
