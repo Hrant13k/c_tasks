5786343927eabb45278b5cba3c4c694393c465e6
Hrant <hrantk1313@gmail.com>
2025-11-27 16:30:00 +0400

    01 tasksv3

---
+++ c01/ex03/ft_div_mod.c
@@ -0,0 +1,26 @@
+//#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+/* 
+int	main(void)
+{
+	int	a = 45;
+	int	b = 5;
+	int	v1 = 0;
+	int	v2 = 0;
+	int	*div = &v1;
+	int	*mod = &v2;
+
+	printf("Before ft_div_mod: \n");
+	printf("Pointer 'div' points to adress %p that has the value %d stored\n", div, *div);
+	printf("Pointer 'mod' points to adress %p that has the value %d stored\n", mod, *mod);
+	ft_div_mod(a, b, div, mod);
+	printf("\n");
+	printf("After ft_div_mod: \n");
+	printf("Pointer 'div' remains pointing to adress %p, but now that has the value %d stored\n", div, *div);
+	printf("Pointer 'mod' remains pointing to adress %p, but now that has the value %d stored\n", mod, *mod);
+} */
---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,45 @@
+//#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	tmp;
+
+	i = 0;
+
+	size--;
+	while (i < size)
+	{
+		tmp = tab[i];
+		tab[i] = tab[size];
+		tab[size] = tmp;
+
+		i++;
+		size--;
+	}
+}
+/* 
+int	main(void)
+{
+	int	tab[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int	size = 10;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	printf("Array before: \n");
+	while (i < size)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+	printf("\n");
+	printf("\nArray after: \n");
+	ft_rev_int_tab(tab, size);
+	while (j < size)
+        {
+		printf("%d ", tab[j]);
+		j++;
+	}
+} */
